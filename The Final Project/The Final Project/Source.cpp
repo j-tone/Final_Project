7ac54c50f3a98a838d209064094f39d77f3c7f43
Ian Stenerson <istenerson17@users.noreply.github.com>
2014-06-23 15:09:22 -0700

    First work
    Do you think if we broke into separate functions like that it would work? That way we can each work on our questions and text files separately.

--- The Final Project/The Final Project/Source.cpp
+++ The Final Project/The Final Project/Source.cpp
@@ -2,9 +2,34 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include "Character.h"
 using namespace std;
 
 int main()
 {
+	string answer;
+	cout << "Do you prefer freedom or subjugation?" << endl;
+	cout << "Answer: ";
+	cin >> answer;
 
+	if (answer == "freedom" || answer == "Freedom" || answer == "FREEDOM")
+	{
+		Hero();
+	}
+
+	else if (answer == "subjugation" || answer == "Subjugation" || answer == "SUBJUGATION")
+	{
+		Villain();
+	}
+}
+
+void Hero() //Ian's code
+{
+  Character Hero;
+  
+}
+
+void Villain() //Justin's code
+{
+  Character Villain
 }
