8612d3231f8f4fa954a4a529346ec1c706bbf55e
j-tone <jtoney17@my.whitworth.edu>
2014-06-26 17:26:52 -0700

    FINALIZED W/IAN'S CODE
    It's all finished, all cohesive now

--- The Final Project/The Final Project/Source.cpp
+++ The Final Project/The Final Project/Source.cpp
@@ -30,230 +30,78 @@ int main()
 
 void Hero() //Ian's code
 {
-  Character Hero;
+	int exists = 1; //Because I had to make more questions to make all the heroes accurate, the user could potentially 
+	//enter a barcode that does not match with a character in the text file.
+	do{
+		Character Hero;
 
-	static int QisA;
-	string Human;
-	string Space;
-	string Supernatural;
-	string Government;
-	string DarkSide;
-	string BatmanSyndrome;
-	string RobinSyndrome;
-	string PewPew;
-	string NickFlamell;
-	string Hiccup;
-	string Destiny;
-	
-	do{ //Human?
-		cout << "Are you Human? Yes or No?" << endl;
-		cin >> Human;
-		QisA = 0;
-		if (Human == "Yes" || Human == "YES" || Human == "yes")
+		vector<Character> UsersHero;
+		vector<string> questions;
+		string line;
+		string HeroFileBase = ("C:\\Users\\Ian\\CS172\\");
+		ifstream HeroNames(HeroFileBase + "CharacterHeroes.txt");
+		int c = 2;
+
+
+		if (!HeroNames)
 		{
-
-			QisA++;
+			cout << "You are not worth of being a Hero. File wont open." << endl;
 		}
-		else if (Human == "No" || Human == "NO" || Human == "no")
-		{
 
-			QisA++;
+		while (getline(HeroNames, line))
+		{
+			if ((c % 2) == 0)
+			{
+				Hero.setName(line);
+				c++;
+			}
+			else
+			{
+				Hero.setBar(line);
+				UsersHero.push_back(Hero);
+				c++;
+			}
+		}
+
+		HeroNames.clear();
+		HeroNames.close();
+
+		ifstream HeroQuestions(HeroFileBase + "HeroesQuestions.txt");
+		vector <char> FirstA, SecondA;
+
+		while (getline(HeroQuestions, line))
+		{
+			questions.push_back(line);
+			int Y = line.size() - 8; //because I formatted my question text file differently, these numbers
+			//account for the spaces I added.
+			int N = line.size() - 3;
+			FirstA.push_back(line.at(Y));
+			SecondA.push_back(line.at(N));
+		}
+
+		HeroQuestions.clear();
+		HeroQuestions.close();
+
+		for (int i = 0; i < questions.size(); i++)
+		{
+			cout << questions[i] << endl;
+			makeAChoice(FirstA, SecondA, Hero, i);
+		}
+
+		evaluation(Hero, UsersHero);
+		cout << endl;
+		if (Hero.getName() == " ")
+		{
+			cout << "There is not a hero in our database that fits your data" << endl;
+			cout << "Try again" << endl;
+			Hero.setBar(" ");//This will reset the barcode to nothing so that the user can try again.
 		}
 		else
 		{
-			cout << "That was not an acceptable response. Try again" << endl;
+			cout << "Your heroic doppleganger is: " << Hero.getName() << "!" << endl;
+			exists--;//removes the user from the loop.
 		}
-	} while (QisA != 1);
-
-	do{ //Space ok?
-		cout << "Is a potential loss of gravity a problem? Yes or No?" << endl; //if no (Star Wars, Star Trek, Marvel)
-		cin >> Space;
-		if (Space == "Yes" || Space == "YES" || Space == "yes")
-		{
-
-			QisA++;
-		}
-		else if (Space == "No" || Space == "NO" || Space == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 2);
-
-	do{ //Supernatural or Natural?
-		cout << "Do you believe in the Natural or the Supernatural?" << endl;
-		cin >> Supernatural;
-		if (Supernatural == "Yes" || Supernatural == "YES" || Supernatural == "yes")
-		{
-
-			QisA++;
-		}
-		else if (Supernatural == "No" || Supernatural == "NO" || Supernatural == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 3);
-
-	do{//Government or Military
-		cout << "Do you have affiliations with the government or military? Yes or No?" << endl;
-		cin >> Government;
-		if (Government == "Yes" || Government == "YES" || Government == "yes")
-		{
-
-			QisA++;
-		}
-		else if (Government == "No" || Government == "NO" || Government == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 4);
-	
-	do{//Potential Dark Side?
-		cout << "Could you be mistaken for a villain? Yes or No?" << endl;
-		cin >> DarkSide;
-		if (DarkSide == "Yes" || DarkSide == "YES" || DarkSide == "yes")
-		{
-
-			QisA++;
-		}
-		else if (DarkSide == "No" || DarkSide == "NO" || DarkSide == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 5);
-
-	do{//Living Parents?
-		cout << "Are your parents still living? Yes or No?" << endl;
-		cin >> BatmanSyndrome;
-		if (BatmanSyndrome == "Yes" || BatmanSyndrome == "YES" || BatmanSyndrome == "yes")
-		{
-
-			QisA++;
-		}
-		else if (BatmanSyndrome == "No" || BatmanSyndrome == "NO" || BatmanSyndrome == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 6);
-
-	do{//Leader or Follower?
-		cout << "Are you a follower or a leader?" << endl;
-		cin >> RobinSyndrome;
-		if (RobinSyndrome == "Yes" || RobinSyndrome == "YES" || RobinSyndrome == "yes")
-		{
-
-			QisA++;
-		}
-		else if (RobinSyndrome == "No" || RobinSyndrome == "NO" || RobinSyndrome == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 7);
-
-	do{//Gun or Sword?
-		cout << "Do you prefer close combat over distance?" << endl;
-		cin >> PewPew;
-		if (PewPew == "Yes" || PewPew == "YES" || PewPew == "yes")
-		{
-
-			QisA++;
-		}
-		else if (PewPew == "No" || PewPew == "NO" || PewPew == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 8);
-
-	do{//Mortal
-		cout << "Are you mortal? Yes or No?" << endl;
-		cin >> NickFlamell;
-		if (NickFlamell == "Yes" || NickFlamell == "YES" || NickFlamell == "yes")
-		{
-
-			QisA++;
-		}
-		else if (NickFlamell == "No" || NickFlamell == "NO" || NickFlamell == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 9);
-
-	do{//Gentle or Agressive?
-		cout << "Are you generally more gentle or more agressive?" << endl;
-		cin >> Hiccup;
-		if (Hiccup == "Yes" || Hiccup == "YES" || Hiccup == "yes")
-		{
-
-			QisA++;
-		}
-		else if (Hiccup == "No" || Hiccup == "NO" || Hiccup == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 10);
-
-	do{ //Following your destiny?
-		cout << "Are you following your destiny? Yes or No?" << endl;
-		cin >> Destiny;
-		if (Destiny == "Yes" || Destiny == "YES" || Destiny == "yes")
-		{
-
-			QisA++;
-		}
-		else if (Destiny == "No" || Destiny == "NO" || Destiny == "no")
-		{
-
-			QisA++;
-		}
-		else
-		{
-			cout << "That was not an acceptable response. Try again" << endl;
-		}
-	} while (QisA != 11);
+	} while (exists = 1);
 }
 
 void Villain() //Justin's code
@@ -262,7 +110,7 @@ void Villain() //Justin's code
 	Character Villain;
 	//this vector will store all characters from the file
 	vector<Character> storedCharacters;
-	//this vector stores all my questions
+	//this vector stores all the questions
 	vector<string> questions;
 	//the string "line" will be very useful
 	string line, file_location = "C:\\Users\\Justin\\Documents\\GitHub\\Final_Project\\The Final Project\\";
@@ -273,7 +121,7 @@ void Villain() //Justin's code
 
 	//if it can't find it it'll let us know
 	if (!afile){
-		cout << "Can't open the fricken damn file." << endl;
+		cout << "You know what's not so great? The file requested can't open. That sucks." << endl;
 	}
 
 	//so, for each line it gets from the file with all of the characters,
